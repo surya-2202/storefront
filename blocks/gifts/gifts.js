a6e026a545712594849a6f4725c038ab77ba15df
surya-2202 <suryakant.makwana@brainvire.com>
2024-06-21 19:30:54 +0530

    Homepage added custom block

---
+++ blocks/gifts/gifts.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-${cols.length}-cols`);
+
+  // setup image columns
+  [...block.children].forEach((row, index) => {
+    const rowClasses = `row-${index + 1}`;
+    row.classList.add('gift-inner-wrapper', rowClasses);
+    
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('columns-img-col');
+        }
+      }
+    });
+  });
+}
